b3eccd3c37808aa5168a43f0c666cf0d1cd32ec9
schani <schani3@naver.com>
2023-05-17 20:58:44 +0900

    알파고 1일차 코드

---
+++ 01_JavaScript 기본 문법/송찬영/대문자로바꾸기.js
@@ -0,0 +1,12 @@
+function solution(my_string, alp) {
+  return my_string
+    .split("")
+    .map((el) => {
+      if (el === alp) {
+        return el.toUpperCase();
+      } else {
+        return el;
+      }
+    })
+    .join("");
+}
