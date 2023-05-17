f0daab5e624b894da87f66994b0e49011409a459
hanbinchoi <wkghskak@naver.com>
2023-05-17 20:27:45 +0900

    삼각형의 완성조건(1)

---
+++ 01_JavaScript 기본 문법/최한빈/삼각형의 완성조건(1).js
@@ -0,0 +1,30 @@
+// [문제 이름]
+// : 삼각형의 완성조건 (1)
+
+// [문제 설명]
+// : 선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+// 가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+// 삼각형의 세 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 세 변으로 삼각형을 만들 수 있다면 1, 만들 수 없다면 2를 return하도록 solution 함수를 완성해주세요.
+
+// [문제 링크]
+// : https://school.programmers.co.kr/learn/courses/30/lessons/120889
+
+function solution(sides) {
+  let sum = 0;
+  const max_index = sides.indexOf(Math.max(...sides));
+  for (let i = 0; i < sides.length; i++) {
+    if (i === max_index) {
+      continue;
+    } else {
+      sum += sides[i];
+    }
+  }
+  if (sum <= sides[max_index]) {
+    return 2;
+  } else {
+    return 1;
+  }
+}
+console.log(solution([1, 2, 3]));
+console.log(solution([3, 6, 2]));
+console.log(solution([199, 72, 222]));
