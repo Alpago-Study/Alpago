c8db8d547962f187cb8ed90a37a4a436ae443690
WONHO22 <dnjsgh2122@gmail.com>
2023-05-25 15:04:29 +0900

    리팩토링

--- 01_JavaScript 기본 문법/이원호/(2주차)시저 암호.js
+++ 01_JavaScript 기본 문법/이원호/(2주차)시저 암호.js
@@ -8,86 +8,42 @@
 // : https://school.programmers.co.kr/learn/courses/30/lessons/12926
 
 function solution(s, n) {
-  const alphabet = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-  ]; //26개
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'; // 알파벳 문자열
+  const alphabetLength = alphabet.length;
 
-  // 일단 문자열을 소문자로 바꾸기
-  let s_lower = s.toLowerCase();
-
-  // s문자열문 대문자인 부분의 인덱스를 index_upper 변수에 저장
-  let index_upper = [];
-
-  // 일단 문자열모두를 소문자로 바꾸고 대문자의 인덱스는 따로 저장해서 기억해두고
-  // 나중에 다시 대문자로 바꿔줄거임
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] !== s_lower[i]) {
-      index_upper.push(i);
-    }
-  }
-
-  // s문자열의 알파벳 순서를 담은 인덱스를 배열에 담을거임
-  let s_converted = [];
-
-  for (let i = 0; i < s_lower.length; i++) {
-    if (s_lower[i] === ' ') {
-      s_converted[i] = ' ';
-    } else if (alphabet.indexOf(s_lower[i]) >= 0) {
-      s_converted[i] = alphabet.indexOf(s_lower[i]);
-    }
-  }
-  // 여기까지 하면
-  // ex) s = "a b z"
-  // s_converted = [0,' ', 1, ' ',25]
-  // 스페이스면 스페이스 반환, 숫자는 알파벳 인덱스를 통해 알파벳 반환
   let result = '';
 
   for (let i = 0; i < s.length; i++) {
-    if (s_converted[i] === ' ') {
+    let char = s[i];
+
+    if (char === ' ') {
+      // 공백 문자인 경우 그대로 결과에 추가
       result += ' ';
-    } else if (s_converted[i] >= 0) {
-      const newIndex = (s_converted[i] + n) % alphabet.length; // 순환해야하기 때문에 알파벳 길이로 나눠줘야함
-      result += alphabet[newIndex];
+      continue; // 다음 반복으로 건너뛰기
+    }
+
+    // 대문자였던 s[i] 파악을 위해 true / false로 저장해둠
+    const isUpperCase = char !== char.toLowerCase();
+
+    // s[i] 일단 소문자로 바꿔주기
+    char = char.toLowerCase();
+
+    // s[i]의 알파벳 순서를 currentIndex에 저장 ex)"a B z" => "0 2 24"
+    const currentIndex = alphabet.indexOf(char);
+
+    if (currentIndex >= 0) {
+      const newIndex = (currentIndex + n) % alphabetLength; // 순환을 위해 알파벳 길이로 나눈 나머지값 ex) 24+4 = 28 % 25의 나머지 3 => d
+      let newChar = alphabet[newIndex];
+
+      if (isUpperCase) {
+        // 원래 대문자였던 경우 대문자로 변환하여 결과에 추가
+        newChar = newChar.toUpperCase();
+      }
+
+      result += newChar;
     }
   }
 
-  // result 결과는 다 소문자로 담김
-  // index_upper 를 활용해서 대문자였던 얘를 대문자로 변환
-
-  for (let i = 0; i < index_upper.length; i++) {
-    if (index_upper[i] >= 0) {
-      // 대문자 인덱스 전까지 자르고, 대문자로 변환하고, 대문자 변환 이후 값 그대로 넣기
-      result =
-        result.substring(0, index_upper[i]) +
-        result[index_upper[i]].toUpperCase() +
-        result.substring(index_upper[i] + 1);
-    }
-  }
   return result;
 }
 
