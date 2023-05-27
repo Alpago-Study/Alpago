3023f6b51bc0982da29df009c67baf600ccd321d
toystory23 <ghlduq@naver.com>
2023-05-27 16:54:13 +0900

    실패율 배열 => 객체로 변환

--- 01_JavaScript 기본 문법/정회엽/실패율.js
+++ 01_JavaScript 기본 문법/정회엽/실패율.js
@@ -13,18 +13,21 @@
 전체 스테이지의 개수 N, 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages가 매개변수로 주어질 때, 
 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return 하도록 solution 함수를 완성하라. */
 
+//문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/42889
+
 function solution(N, stages) {
   let stagesLength = stages.length;
   let failGroup = [];
   for (let i = 1; i <= N; i++) {
     let failNum = stages.filter((v) => v === i).length;
-    failGroup.push([i, failNum / stagesLength]);
+    failGroup.push({ stage: i, failRate: failNum / stagesLength });
     stagesLength -= failNum;
   }
-  let result = failGroup.sort((a, b) => b[1] - a[1]);
+  let result = failGroup.sort((a, b) => b.failRate - a.failRate);
 
-  return result.map((v) => v[0]);
+  return result.map((v) => v.stage);
 }
+//.map(value, function)
 
 console.log(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]));
 console.log(solution(4, [4, 4, 4, 4, 4]));
