c8e776c432d6509c188f56c7e3d762a5f3b20920
hanbinchoi <44600584+hanbinchoi@users.noreply.github.com>
2023-06-11 14:52:52 +0900

    test

--- 03_그리디(탐욕법)/최한빈/example.js
+++ 03_그리디(탐욕법)/최한빈/example.js
@@ -1,16 +1,42 @@
-// [문제 이름]
-// : 두 수의 곱
-
-// [문제 설명]
-// : 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
-
-// [문제 링크]
-// : https://school.programmers.co.kr/learn/courses/30/lessons/120804
-
-function solution(num1, num2) {
-  const answer = 0;
-  return answer;
+function left(n,lost,reserve) {
+   var answer = n-lost.length;
+    for (i of lost) {
+        if (reserve.find(ele => ele===i-1)) {
+            answer++;
+            reserve = reserve.filter(ele=>ele!==i-1)
+        }
+        else if (reserve.find(ele=>ele===i+1)) {
+            answer++;
+            reserve = reserve.filter(ele=>ele!==i+1)
+        }
+    }
+    return answer; 
+}
+function right(n,lost,reserve) {
+   var answer = n-lost.length;
+    for (i of lost) {
+        if (reserve.find(ele => ele===i+1)) {
+            answer++;
+            reserve = reserve.filter(ele=>ele!==i+1)
+        }
+        else if (reserve.find(ele=>ele===i-1)) {
+            answer++;
+            reserve = reserve.filter(ele=>ele!==i-1)
+        }
+    }
+    return answer; 
 }
 
-console.log(solution(3, 4));
-console.log(solution(27, 19));
+function solution(n, lost, reserve) {
+    const both = []
+    lost.forEach((ele) => {
+        if(reserve.includes(ele)) {
+            both.push(ele)
+        }
+    })
+    lost = lost.filter((ele) => !both.includes(ele))
+    reserve = reserve.filter((ele) => !both.includes(ele))
+    lost.sort((a,b)=>a-b);
+    reserve.sort((a,b)=>a-b);
+    return Math.max(left(n,lost,reserve),right(n,lost,reserve))
+}
