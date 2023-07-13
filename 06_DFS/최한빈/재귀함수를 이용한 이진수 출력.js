f101c1be02045826e4536b617284d51244d8aa56
hanbinchoi <wkghskak@naver.com>
2023-07-13 17:15:52 +0900

    수정

--- 06_DFS/최한빈/재귀함수를 이용한 이진수 출력.js
+++ 06_DFS/최한빈/재귀함수를 이용한 이진수 출력.js
@@ -10,6 +10,7 @@
 function printBinaryNumber(num) {
   // 2진수 변환 -> 수를 2로 나눈 후 나머지는 맨 뒤, 몫은 다시 2로 나눈다.(수가 1이 될 떄까지)
   if (num === 1) return '1';
+  else if (num === 0) return '0';
   return printBinaryNumber(parseInt(num / 2)) + ('' + (num % 2));
 }
 console.log(printBinaryNumber(11));
