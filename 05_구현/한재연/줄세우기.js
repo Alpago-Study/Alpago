aee20a7779fc94de17cc6a191b86f455f64ad72e
Jess-Apr <jessie1944@naver.com>
2023-07-22 17:07:48 +0900

    (구현) 올림픽 문제 풀이

--- 05_구현/한재연/줄세우기.js
+++ 05_구현/한재연/줄세우기.js
@@ -11,37 +11,38 @@
 // [문제 링크]
 // : https://www.acmicpc.net/problem/10431
 
-
 // 백준 제출용 코드
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let data = input.map(el => el.split(' ')).map(el => el.map(Number).slice(1));
+let data = input
+  .map((el) => el.split(' '))
+  .map((el) => el.map(Number).slice(1));
 data.shift();
 
 // 내 앞에 서있는 나보다 작은 친구들은 앞 순서에서 나보다 앞으로 이동할 것이기 때문에 결론적으로는 내 앞에 나보다 큰 학생들의 수만 세면 뒤로 물러서는 수를 계산할 수 있음.
 function solution(data) {
-    // 각각의 케이스 정답을 담을 배열
-    let resultArr = [];
+  // 각각의 케이스 정답을 담을 배열
+  let resultArr = [];
 
-    // 각각의 케이스를 하나씩 도는 반복문
-    for (let i = 0; i < data.length; i++) {
-        let line = data[i];
-        // 각각의 케이스는 개별적으로 계산해야 하기 때문에 케이스가 달라질때마다 count값을 0으로 초기화해준다.
-        let count = 0;
-        // j 인덱스 학생 앞에 j 인덱스 학생보다 키가 큰 학생의 수를 구하는 반복문
-        for (let j = 0; j < data[i].length; j++) {
-            for (let k = 0; k < j; k++) {
-                // j 인덱스 학생 앞에 더 큰 키를 가진 학생이 있다면 count 수를 하나 올려준다.
-                if (line[k] > line[j]) count++;
-            }
-        }
-        // 하나의 케이스 계산이 끝나면 배열에 순서와 count 수를 담아준다.
-        resultArr = [...resultArr, `${i + 1} ${count}`];
+  // 각각의 케이스를 하나씩 도는 반복문
+  for (let i = 0; i < data.length; i++) {
+    let line = data[i];
+    // 각각의 케이스는 개별적으로 계산해야 하기 때문에 케이스가 달라질때마다 count값을 0으로 초기화해준다.
+    let count = 0;
+    // j 인덱스 학생 앞에 j 인덱스 학생보다 키가 큰 학생의 수를 구하는 반복문
+    for (let j = 0; j < data[i].length; j++) {
+      for (let k = 0; k < j; k++) {
+        // j 인덱스 학생 앞에 더 큰 키를 가진 학생이 있다면 count 수를 하나 올려준다.
+        if (line[k] > line[j]) count++;
+      }
     }
+    // 하나의 케이스 계산이 끝나면 배열에 순서와 count 수를 담아준다.
+    resultArr = [...resultArr, `${i + 1} ${count}`];
+  }
 
-    // 포맷에 맞추어 정답 출력
-    return resultArr.join("\n");
+  // 포맷에 맞추어 정답 출력
+  return resultArr.join('\n');
 }
 
 console.log(solution(data));
